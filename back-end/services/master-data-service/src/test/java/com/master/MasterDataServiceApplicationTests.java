f8f50381534d3517c8f8df452b98ac561ec7986d
cvaldiviape <cvaldiviape@gmail.com>
2024-09-08 19:50:32 -0500

    first commit

---
+++ back-end/services/master-data-service/src/test/java/com/master/MasterDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.master;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterDataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ back-end/services/shared-service/src/test/java/com/shared/SharedServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shared;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharedServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
