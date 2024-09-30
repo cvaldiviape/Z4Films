48b179748697c315f2e1d3181e4098236117862a
cvaldiviape <cvaldiviape@gmail.com>
2024-09-29 19:40:59 -0500

    feat (backend)[service-master-data]: implement method FindAllByListIds generic to Country, Genre and Language

--- back-end/services/z4-master-data-service/src/main/java/com/master/core/controller/GenreController.java
+++ back-end/services/z4-master-data-service/src/main/java/com/master/core/controller/GenreController.java
@@ -13,19 +13,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/genres")
 public class GenreController implements IGenreController<GenreDto, Integer> {
 
-    private final GenericFindByIdService<GenreEntity, GenreDto, Integer> findByIdService;
     private final GenericFindAllService<GenreEntity, GenreDto, Integer> findAllService;
+    private final GenericFindAllByListIdsService<GenreEntity, GenreDto, Integer> findAllByListIdsService;
+    private final GenericFindByIdService<GenreEntity, GenreDto, Integer> findByIdService;
     private final GenericCreateService<GenreEntity, GenreDto, Integer> createService;
     private final GenericUpdateService<GenreEntity, GenreDto, Integer> updateService;
     private final GenericDeleteService<GenreEntity, GenreDto, Integer> deleteService;
 
     public GenreController(
             @Qualifier("findAllGenreImpl") GenericFindAllService<GenreEntity, GenreDto, Integer> findAllService,
+            @Qualifier("findAllByListIdsGenreImpl") GenericFindAllByListIdsService<GenreEntity, GenreDto, Integer> findAllByListIdsService,
             @Qualifier("findByIdGenreImpl") GenericFindByIdService<GenreEntity, GenreDto, Integer> findByIdService,
             @Qualifier("createGenreImpl") GenericCreateService<GenreEntity, GenreDto, Integer> createService,
             @Qualifier("updateGenreImpl") GenericUpdateService<GenreEntity, GenreDto, Integer> updateService,
             @Qualifier("deleteGenreImpl") GenericDeleteService<GenreEntity, GenreDto, Integer> deleteService) {
         this.findAllService = findAllService;
+        this.findAllByListIdsService = findAllByListIdsService;
         this.findByIdService = findByIdService;
         this.createService = createService;
         this.updateService = updateService;
@@ -35,6 +38,11 @@ public class GenreController implements IGenreController<GenreDto, Integer> {
     @Override
     public FindAllService<GenreDto> getFindAllService() {
         return this.findAllService;
+    }
+
+    @Override
+    public FindAllByListIdsService<GenreDto, Integer> getFindAllByListIdsService() {
+        return this.findAllByListIdsService;
     }
 
     @Override
