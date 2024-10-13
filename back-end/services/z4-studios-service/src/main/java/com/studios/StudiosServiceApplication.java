956ed34f4a3d4e27a5d775270f6d88460176ee28
cvaldiviape <cvaldiviape@gmail.com>
2024-10-12 21:31:54 -0500

    feat (backend)[service-studios]: add new service "studios-services"

---
+++ back-end/services/z4-studios-service/src/main/java/com/studios/StudiosServiceApplication.java
@@ -0,0 +1,13 @@
+package com.studios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudiosServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudiosServiceApplication.class, args);
+	}
+
+}
