197f32332bc851ee7f86f668066d9822b1fb4daf
cvaldiviape <cvaldiviape@gmail.com>
2024-10-19 21:41:35 -0500

    feat (backend)[nosql-service-catalogs]: add new service z4nosql-catalogs-service

---
+++ back-end/services/z4nosql-catalogs-service/src/main/java/com/nosqlcatalogs/Z4nosqlCatalogsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nosqlcatalogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Z4nosqlCatalogsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Z4nosqlCatalogsServiceApplication.class, args);
+	}
+
+}
